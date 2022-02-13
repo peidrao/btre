62822f4144ef78f80a49122406deb70c6b73cea9
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-13 18:59:57 -0300

    Add: admin models and update date in models

--- listings/models.py
+++ listings/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from django.utils import timezone
 from django.db import models
 
 from realtors.models import Realtor
@@ -18,7 +18,7 @@ class Listing(models.Model):
     bedrooms = models.IntegerField()
     bathrooms = models.IntegerField()
     garage = models.IntegerField(default=0)
-    sqft = models.IntegerField(max_length=200)
+    sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d')
     photo1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
@@ -26,7 +26,7 @@ class Listing(models.Model):
     photo3 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     photo4 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(default=datetime.now(), blank=True)
+    list_date = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self):
         return self.title
--- realtors/models.py
+++ realtors/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from django.utils import timezone
 
 from django.db import models
 
@@ -11,7 +11,7 @@ class Realtor(models.Model):
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
     is_mvp = models.BooleanField(default=False)
-    hire_date = models.DateTimeField(default=datetime.now(), blank=True)
+    hire_date = models.DateTimeField(default=timezone.now, blank=True)
 
     def __str__(self) -> str:
         return self.name
