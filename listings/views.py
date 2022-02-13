b276f6fc73b68804acaf8cfdf455fd8fc5e4ad6d
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-13 20:13:14 -0300

    Add: pagination in listing by id

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
 def listings(request):
-    listings = Listing.objects.filter(is_published=True)
-    context = {'listings': listings}
+    listings = Listing.objects.filter(is_published=True).order_by('-list_date')
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {'listings': paged_listings}
     return render(request, 'listings/listings.html', context)
 
 
