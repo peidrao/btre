e4c958ba39816981a92ec135bf714d9b14af217e
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-13 19:21:45 -0300

    Update: migrations and models in listing and realtor

--- btre/settings.py
+++ btre/settings.py
@@ -108,8 +108,7 @@ STATICFILES_DIRS = [
        BASE_DIR / "btre/static",
 ]
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- realtors/models.py
+++ realtors/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Realtor(models.Model):
     name =  models.CharField(max_length=200)
-    photo =  models.ImageField(upload_to='photos/%Y/%m/%d')
+    photo =  models.ImageField(upload_to='photos_%Y-%m-%d')
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
