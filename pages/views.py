edb2b9f6d4485697cf360859f25bae2d4652b5e3
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-14 21:11:00 -0300

    Update: custom index page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from listings.models import Listing
+
 
 def index(request):
-    return render(request, 'pages/index.html', {})
+    listings = Listing.objects.filter(is_published=True).order_by('-list_date')[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html', {})
