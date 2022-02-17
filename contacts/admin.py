36d195c47ae847ac87494ae675dfd7a24823709b
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-17 20:14:57 -0300

    Add: custom admiN

--- contacts/admin.py
+++ contacts/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from contacts.models import Contact
 
-# Register your models here.
 
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 25
+
+
+admin.site.register(Contact, ContactAdmin)
